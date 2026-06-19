653e7bb5814120460e61add8cdc57288c2dc0aa5
Ieuan Byers <ieuan.byers@justice.gov.uk>
2026-06-19 15:51:00 +0100

    chore: add basic logging to app

--- app/main.py
+++ app/main.py
@@ -1,14 +1,19 @@
+import logging
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from app.routers import root, info, recipes, health
 from app import http_client
 
+logger = logging.getLogger(__name__)
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    logger.info("plum-fastapi-backend starting up")
     await http_client.open_client()
     yield
     await http_client.close_client()
+    logger.info("plum-fastapi-backend shut down")
 
 
 app = FastAPI(
